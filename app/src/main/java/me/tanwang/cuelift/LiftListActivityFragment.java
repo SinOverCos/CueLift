b385045a3af24106a28ab08107b34a67387ba7ec
Tan Wang <tanwang@Tan.local>
2016-07-13 22:06:36 -0400

    TIL
    Check this:
    http://stackoverflow.com/questions/22729330/super-oncreateview-in-fragments
    Classes that extend Fragment don't have their own views (onCreateView
            returns null)
    Classes that extend ListFragment DO have their own views (onCreatView
            returns a view with a listview and everything)
    When extending Fragment: make your own view
    When extending ListFragment: return view from super.onCreateView

--- app/src/main/java/me/tanwang/cuelift/LiftListActivityFragment.java
+++ app/src/main/java/me/tanwang/cuelift/LiftListActivityFragment.java
@@ -1,22 +1,23 @@
 package me.tanwang.cuelift;
 
-import android.support.v4.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class LiftListActivityFragment extends Fragment {
+public class LiftListActivityFragment extends ListFragment {
 
-    public LiftListActivityFragment() {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lift_list, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+
+        return view;
     }
 }
