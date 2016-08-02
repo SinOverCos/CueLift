82cd69257cb395ab929cbd445a2f6303116efe39
Tan Wang <tanw.mini@gmail.com>
2016-08-01 22:35:07 -0400

    datepicker works, need to do rep picker, weightpicker, add to/del from table

--- app/src/main/java/me/tanwang/cuelift/DateDialogFragment.java
+++ app/src/main/java/me/tanwang/cuelift/DateDialogFragment.java
@@ -1,13 +1,19 @@
 package me.tanwang.cuelift;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.DatePicker;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class DateDialogFragment extends DialogFragment {
 
@@ -25,6 +31,40 @@ public class DateDialogFragment extends DialogFragment {
         int day = calendar.get(Calendar.DAY_OF_MONTH);
 
         View view = getActivity().getLayoutInflater().inflate(R.layout.dialog_date, null);
+        DatePicker datePicker = (DatePicker) view.findViewById(R.id.set_date_picker);
+        datePicker.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                setDate = new GregorianCalendar(year, monthOfYear, dayOfMonth).getTime();
+                getArguments().putSerializable(SET_DATE, setDate);
+            }
+        });
+
+        return new AlertDialog.Builder(getActivity())
+                .setView(view)
+                .setTitle(R.string.set_date)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                sendResult(Activity.RESULT_OK);
+                            }
+                        })
+                .create();
     }
 
+    public static DateDialogFragment newInstance(Date date) {
+        Bundle dataPassed = new Bundle();
+        dataPassed.putSerializable(SET_DATE, date);
+        DateDialogFragment fragment = new DateDialogFragment();
+        fragment.setArguments(dataPassed);
+        return fragment;
+    }
+
+    private void sendResult(int result) {
+        if(getTargetFragment() == null) return;
+        Intent intent = new Intent();
+        intent.putExtra(SET_DATE, setDate);
+        getTargetFragment().onActivityResult(getTargetRequestCode(), result, intent);
+    }
 }
--- app/src/main/java/me/tanwang/cuelift/LiftFragment.java
+++ app/src/main/java/me/tanwang/cuelift/LiftFragment.java
@@ -4,10 +4,12 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.LoaderManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.support.design.widget.FloatingActionButton;
 import android.os.Bundle;
+import android.app.FragmentManager;
 import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -16,29 +18,37 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import android.widget.ListAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.IllegalFormatException;
+import java.util.Locale;
 
 public class LiftFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = "LiftFragment";
-    private static final int ID_LOAD_CUES = 1;
     private static final String CUE_LIFT_ID = "me.tanwang.cuelift.cue_lift_id";
+    private static final int ID_LOAD_CUES = 1;
+    private static final int REQUEST_DATE = 10;
 
     private LiftFragmentCallbacks callbacks;
     private LiftManager liftManager;
     private LiftDatabaseHelper.CueCursor cueCursor;
 
     private LinearLayout cueLinearLayout;
+
+    private TextView datePickerTextView;
+    private TextView repPickerTextView;
+    private TextView weightPickerTextView;
 
     private ImageButton liftIconImageButton;
     private EditText liftNameEditText;
@@ -88,15 +98,26 @@ public class LiftFragment extends Fragment implements LoaderManager.LoaderCallba
         lift = (Lift) getArguments().getSerializable(Lift.EXTRA_LIFT);
     }
 
+    public String formatDate(Date date) {
+        String formatted = new SimpleDateFormat("dd MMMM yyyy", Locale.CANADA).format(date);
+        Log.i(TAG, "Formatted date: " + formatted);
+        return formatted;
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_lift, parent, false);
 
-        cueLinearLayout = (LinearLayout) view.findViewById(R.id.cue_list);
         liftIconImageButton = (ImageButton) view.findViewById(R.id.lift_icon_image_button);
         liftNameEditText = (EditText) view.findViewById(R.id.lift_name_edit_text);
+
+        cueLinearLayout = (LinearLayout) view.findViewById(R.id.cue_list);
         addCueEditText = (EditText) view.findViewById(R.id.add_cue_edit_text);
         addCueFab = (FloatingActionButton) view.findViewById(R.id.add_cue_fab);
+
+        datePickerTextView = (TextView) view.findViewById(R.id.date_picker);
+        repPickerTextView = (TextView) view.findViewById(R.id.rep_picker);
+        weightPickerTextView = (TextView) view.findViewById(R.id.weight_picker);
 
         liftIconImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -134,6 +155,23 @@ public class LiftFragment extends Fragment implements LoaderManager.LoaderCallba
 
         loadCues();
 
+        datePickerTextView.setText(formatDate(new Date()));
+        datePickerTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getActivity().getFragmentManager();
+                Date date;
+                try {
+                    date = new SimpleDateFormat("dd MMMM yyyy", Locale.CANADA).parse(datePickerTextView.getText().toString());
+                } catch (ParseException e) {
+                    date = new Date();
+                }
+                DateDialogFragment dateDialogFragment = DateDialogFragment.newInstance(date);
+                dateDialogFragment.setTargetFragment(LiftFragment.this, REQUEST_DATE);
+                dateDialogFragment.show(fragmentManager, getResources().getString(R.string.set_date));
+            }
+        });
+
         // TODO if today's lifts/last day's lifts don't exist, hide that section
         // TODO if no lifts exist at all, hide the PR section
 
@@ -146,6 +184,14 @@ public class LiftFragment extends Fragment implements LoaderManager.LoaderCallba
         Log.i(TAG, "LiftFragment#onResume called");
         if (getUserVisibleHint()) {
             reloadCues();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_DATE) {
+            Date setDate = (Date) data.getSerializableExtra(DateDialogFragment.SET_DATE);
+            datePickerTextView.setText(formatDate(setDate));
         }
     }
 
